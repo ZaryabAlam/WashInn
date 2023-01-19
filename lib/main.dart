fc56d029edd0bc4969552db4bc24f00b91f5cb67
ZaryabAlam <zaryabalam97@gmail.com>
2023-01-19 22:01:16 +0500

    Project Progress

--- lib/main.dart
+++ lib/main.dart
@@ -2,8 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:smooth_page_indicator/smooth_page_indicator.dart';
 import 'package:outline_gradient_button/outline_gradient_button.dart';
-
 import 'screens/home.dart';
+//test
 
 Future main() async {
   WidgetsFlutterBinding.ensureInitialized();
